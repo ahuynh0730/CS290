82cd979c5222ae749655e4da355acb157cacc389
Anthony Huynh <42370685+ahuynh0730@users.noreply.github.com>
2019-09-24 11:18:55 -0500

    Update dbcon.js

--- Week 9 - Workout Tracker/dbcon.js
+++ Week 9 - Workout Tracker/dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit 	: 10,
   host            	: 'remotemysql.com',
-  user 				: 'gNDRuQR4wv',
-  password			: '7xGAgwmBDI',
-  database			: 'gNDRuQR4wv' 
+  user 			      	: '(insert own user here)',
+  password		    	: '(insert own password here)',
+  database			    : '(insert own database here)' 
 });
 
 module.exports.pool = pool;
